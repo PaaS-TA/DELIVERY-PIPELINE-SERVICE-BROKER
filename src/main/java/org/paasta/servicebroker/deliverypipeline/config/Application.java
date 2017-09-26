456f7e3e356d86bd996c4a62a845d3e1d145211e
REX <rexx4314@gmail.com>
2017-09-26 15:26:20 +0900

    modify comment

--- src/main/java/org/paasta/servicebroker/deliverypipeline/config/Application.java
+++ src/main/java/org/paasta/servicebroker/deliverypipeline/config/Application.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
     public static void main(String[] args) {
+        // TODO
         SpringApplication.run(Application.class, args);
     }
     	
