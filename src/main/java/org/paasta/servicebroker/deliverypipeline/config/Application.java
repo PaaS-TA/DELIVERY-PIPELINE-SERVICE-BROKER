e4d56ee727cae396dc2a6544b6f80b3ee8a74d09
REX <rexx4314@gmail.com>
2017-09-26 15:37:21 +0900

    remove unused file and revert test comments

--- src/main/java/org/paasta/servicebroker/deliverypipeline/config/Application.java
+++ src/main/java/org/paasta/servicebroker/deliverypipeline/config/Application.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO
         SpringApplication.run(Application.class, args);
     }
     	
