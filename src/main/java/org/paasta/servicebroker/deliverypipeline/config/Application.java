fa88e6a7407122fcdb016bdae3b59b584da749e2
swmoon <swmoon@bluedigm.com>
2017-11-14 14:56:16 +0900

    플랜 추가로 테스트 케이스 수정 및 플랜 추가

--- src/main/java/org/paasta/servicebroker/deliverypipeline/config/Application.java
+++ src/main/java/org/paasta/servicebroker/deliverypipeline/config/Application.java
@@ -7,9 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 @ComponentScan
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    	
 }
